c90f92d2a0eb6d103e4326f903b312a1961fa9b4
문석희/전시DX팀 <zazachucky@gsretail.com>
2026-03-19 17:45:37 +0900

    avif_ios 변경

--- flutter_avif_ios/lib/flutter_avif_ios.dart
+++ flutter_avif_ios/lib/flutter_avif_ios.dart
@@ -4,6 +4,9 @@ import 'package:flutter_avif_platform_interface/flutter_avif_platform_interface.
 
 class FlutterAvifIos extends FlutterAvifPlatform {
   static void registerWith() {
-    FlutterAvifPlatform.api = FlutterAvifImpl(DynamicLibrary.executable());
+    // FlutterAvifPlatform.api = FlutterAvifImpl(DynamicLibrary.executable());
+    // iOS에서 store_dart_post_cobject 등 Dart VM 심볼은 executable()에 없음.
+    // process()로 조회해야 함 (flutter_avif #24, flutter_rust_bridge #898).
+    FlutterAvifPlatform.api = FlutterAvifImpl(DynamicLibrary.process());
   }
 }
